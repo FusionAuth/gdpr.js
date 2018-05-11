89d215eaf01df6fcf991342bf4041211f8426c19
Brian Pontarelli <brian@inversoft.com>
2018-05-11 16:16:19 -0600

    Added reset and styling

--- docs/js/gdpr.js
+++ docs/js/gdpr.js
@@ -10,6 +10,7 @@ var GDPR = function(element, euURL) {
 
   // Check for existing answers
   var existingAnswer = this._loadAnswer();
+  console.debug('Answer is ' + existingAnswer);
   if (existingAnswer === null) {
     this._openModal();
   } else if (existingAnswer === 'yes') {
@@ -20,6 +21,7 @@ var GDPR = function(element, euURL) {
 GDPR.prototype = {
   _handleNoClick: function() {
     this._storeAnswer('no');
+    this.overlay.parentNode.removeChild(this.overlay);
     this.element.style.display = 'none';
     document.documentElement.style.overflow = '';
   },
@@ -38,9 +40,9 @@ GDPR.prototype = {
     document.documentElement.style.overflow = 'hidden';
 
     // Create the overlay
-    var overlay = document.createElement('DIV');
-    overlay.setAttribute('style', 'background: rgba(0, 0, 0, 0.5); display: block; height: 100%; left: 0; position: fixed; top: 0; width: 100%; z-index: 9999');
-    document.body.insertBefore(overlay, document.body.firstChild);
+    this.overlay = document.createElement('DIV');
+    this.overlay.setAttribute('style', 'background: rgba(0, 0, 0, 0.5); display: block; height: 100%; left: 0; position: fixed; top: 0; width: 100%; z-index: 9999');
+    document.body.insertBefore(this.overlay, document.body.firstChild);
 
     // Setup the dialog
     var yesButton = this.element.querySelector('.yes-button');
